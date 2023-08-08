f5671cc0ec06b9b3f633627f6527f46c80072e7d
eerrbis@gmail.com <trimurs@gmail.com>
2023-08-09 00:02:02 +0300

    добавлена проверка на пустые поля

--- firsttask/serializers.py
+++ firsttask/serializers.py
@@ -5,25 +5,19 @@ from rest_framework import serializers
 class DataSerializer(serializers.Serializer):
     name = serializers.CharField()
     message_template = serializers.CharField()
+    type = serializers.CharField()
     scopes = serializers.ListField(child=serializers.DictField())
 
     def validate(self, attrs):
         message_template = attrs['message_template']
         pattern = r'\[([^\]]+)\]'
         variable_list = re.findall(pattern, message_template)
-        variable_dict = {}
-        try:
-            for variable in variable_list:
-                key, value = variable.split('.') if '.' in variable else (variable, None)
-                variable_dict.setdefault(key, {})[value] = True
-        except:
-            raise serializers.ValidationError(f"Неверный формат scopes")
-
+        variable_dict = self.process_data(variable_list)
         for scope in attrs['scopes']:
             if (result := self.find_key_difference(variable_dict, scope)) and result is not None:
                 raise serializers.ValidationError([f"Scope: {scope}",f"Error: Отсутствуют ключи {','.join(result)}"])
 
-
+        self.check_empty_values(attrs)
         return attrs
 
     def find_key_difference(self,dict1, dict2):
@@ -37,4 +31,35 @@ class DataSerializer(serializers.Serializer):
 
         return missing_keys
 
+    def process_data(self,variable_list):
+        variable_dict = {}
+        for variable in variable_list:
+            keys = variable.split('.')
+            current_dict = variable_dict
+            for i, key in enumerate(keys):
+                if i == len(keys) - 1:
+                    current_dict[key] = True
+                else:
+                    current_dict = current_dict.setdefault(key, {})
+        return variable_dict
+
+    def check_empty_values(self, attrs):
+        for key, value in attrs.items():
+            if isinstance(value, str) and not value.strip():
+                raise serializers.ValidationError(
+                    f"Значение для поля '{key}' не может быть пустым или состоять только из пробелов.")
+            elif isinstance(value, list):
+                for item in value:
+                    self.check_empty_values(item)
+            elif isinstance(value, dict):
+                self.check_empty_values(value)
+
+    def validate_type(self, value):
+        """
+        Проверка, что поле Type содержит только "WhatsApp" или "email".
+        """
+        valid_types = ["WhatsApp", "email"]
+        if value not in valid_types:
+            raise serializers.ValidationError(f"Поле Type может принимать только значения: {', '.join(valid_types)}.")
+        return value
 
