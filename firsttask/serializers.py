190c68c404c1859207767d3a4e4386cff7da81fa
eerrbis@gmail.com <trimurs@gmail.com>
2023-08-08 18:09:57 +0300

    исправлено: в scopes могут быть любые переменные добавлена валидация

--- firsttask/serializers.py
+++ firsttask/serializers.py
@@ -1,15 +1,40 @@
+import re
 
 from rest_framework import serializers
 
-class EstateSerializer(serializers.Serializer):
-    address = serializers.CharField(max_length=200)
-    sq = serializers.IntegerField()
+class DataSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    message_template = serializers.CharField()
+    scopes = serializers.ListField(child=serializers.DictField())
 
-class ScopeSerializer(serializers.Serializer):
-    retail = serializers.CharField()
-    estate = EstateSerializer()
+    def validate(self, attrs):
+        message_template = attrs['message_template']
+        pattern = r'\[([^\]]+)\]'
+        variable_list = re.findall(pattern, message_template)
+        variable_dict = {}
+        try:
+            for variable in variable_list:
+                key, value = variable.split('.') if '.' in variable else (variable, None)
+                variable_dict.setdefault(key, {})[value] = True
+        except:
+            raise serializers.ValidationError(f"Неверный формат scopes")
 
-class DispatchCreateSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
-    scopes = ScopeSerializer(many=True)
-    message_template = serializers.CharField(max_length=200)
+        for scope in attrs['scopes']:
+            if (result := self.find_key_difference(variable_dict, scope)) and result is not None:
+                raise serializers.ValidationError([f"Scope: {scope}",f"Error: Отсутствуют ключи {','.join(result)}"])
+
+
+        return attrs
+
+    def find_key_difference(self,dict1, dict2):
+        missing_keys = set()
+        for key in dict1.keys():
+            if key not in dict2:
+                missing_keys.add(key)
+            elif isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
+                nested_missing_keys = self.find_key_difference(dict1[key], dict2[key])
+                missing_keys.update({f"{key}.{nested_key}" for nested_key in nested_missing_keys})
+
+        return missing_keys
+
+
--- firsttask/views.py
+++ firsttask/views.py
@@ -5,23 +5,22 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from firsttask.models import Dispatch
-from firsttask.serializers import DispatchCreateSerializer
+from firsttask.serializers import DataSerializer
 
+from jinja2 import Template
 
 class ScheduleCreateView(APIView):
     def post(self, request):
-        serializer = DispatchCreateSerializer(data=request.data)
+        serializer = DataSerializer(data=request.data)
         if serializer.is_valid():
             name = serializer.validated_data['name']
-            message_template = serializer.validated_data['message_template'].replace('[','{').replace(']','}')
+            message_template = serializer.validated_data['message_template'].replace('[','{{').replace(']','}}')
             scopes = serializer.validated_data['scopes']
             dispatches_to_create = []
             for scope in scopes:
-                retail_name = scope['retail']
-                estate_address = scope['estate']['address']
-                square = scope['estate']['sq']
-                message = message_template.format(retail_name=retail_name, estate_address=estate_address, estate_square=square)
-                dispatches_to_create.append(Dispatch(name=name, message=message))
+                template = Template(message_template)
+                formatted_message = template.render(**scope)
+                dispatches_to_create.append(Dispatch(name=name, message=formatted_message))
             Dispatch.objects.bulk_create(dispatches_to_create)
             return Response("Data saved successfully", status=status.HTTP_201_CREATED)
         else:
